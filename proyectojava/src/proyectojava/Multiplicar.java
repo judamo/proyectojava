5eb9f8f03f8348ef91b19d00f5d67547b26efcfd
chocosoft1 <wmaster@eam.edu.co>
2016-08-23 01:10:40 -0500

    Multiplicar

---
+++ proyectojava/src/proyectojava/Multiplicar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectojava;
+
+/**
+ *
+ * @author 414pc03
+ */
+public class Multiplicar {
+  /**
+   * Para Multiplicar
+   * @param a
+   * @param b
+   * @return 
+   */  
+
+    public int Multiplica(int a,int b){
+    return a*b;
+    
+    }
+}
+    
