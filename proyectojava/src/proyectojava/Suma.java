e6b57aa5d13c1dbe476d0b09f31faab7f56dbb4b
Juan Montoya <jdm@eam.edu.co>
2016-08-23 15:34:43 -0500

    crear clase suma

---
+++ proyectojava/src/proyectojava/Suma.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectojava;
+
+/**
+ *
+ * @author juan
+ */
+public class Suma {
+    
+    
+    public int obtenerSuma(int a,int b){
+    return a+b;
+    }
+    
+}
